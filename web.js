c41b5ed3e72d1dc6341d8c770e73512f7366de6e
Eric Lui <eric.lui@xtremelabs.com>
2013-07-13 10:58:55 -0400

    EricL - Added index page and mocked single page web app functionality. Also added basic Facebook authentication.

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require("express");
 var app = express();
-app.use(express.logger());
 
+// Routing
 app.get('/', function(request, response) {
-	response.send('Hello World!');
+	response.sendfile(__dirname + '/index.html');
+});
+app.get('/test1', function(request, response) {
+	response.send('test page one');
+});
+app.get('/test2', function(request, response) {
+	response.send('test page two');
 });
 
 var port = process.env.PORT || 5000;
